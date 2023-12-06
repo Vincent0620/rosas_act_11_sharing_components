e57884deb75f99f995cf642c968d8e7137a19896
Vincent0620 <marcusvincentrosas@thelewiscollege.edu.ph>
2023-12-06 23:00:27 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
+import {Styles} from './style';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
+function SharingStyleComponents() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={Styles.Body}>
+      <div style={Styles.Body1}>
+        <div style={Styles.Title}>
+          <h3>Sharing Components</h3>
+        </div>
+        <div style={Styles.Container}>
+          <div style={Styles.LoginTitle}>
+            <h3>Login Form</h3>
+          </div>
+          <div>
+            <div style={Styles.BodyContainer}>
+              <form>
+                  <input style={Styles.Input} type="email" placeholder="Email Address" />
+                  <input style={Styles.Input} type="password" placeholder="Password" />
+                  <button style={Styles.Button}>LOGIN</button>
+              </form>
+              <div style={Styles.Half}>
+                <div style={Styles.Horizontal}>
+                  <hr></hr>
+                </div>
+                <div>
+                  <p style={Styles.Opacity}>OR</p>
+                </div>
+                <div style={Styles.Horizontal}>
+                  <hr></hr>
+                </div>
+              </div>
+              <div style={Styles.LogoArrangement}>
+                <a href="#"><img style={Styles.Logo} src={logo}></img></a>
+               
+              </div>
+              <div style={Styles.Footer}>
+                <p>No Account? <a href = "#">Create Account Here!</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+export default SharingStyleComponents;
 
-export default App;
+
---
+++ src/style.js
@@ -0,0 +1,32 @@
+const Body = {width: "auto"};
+const Body1 = {width: "750px", margin: "auto", fontFamily: "Comic Sans MS"};
+const Title = {margin: "10px", textAlign: "Center"};
+const Container = {background: "Orange", padding: "6% 0 0",height: "850px", width: "750px"};
+const LoginTitle = {height: "10%", textAlign: "Center"};
+const BodyContainer = {margin: "10%"};
+const Input = {boxSizing: "border-box",width: "100%",marginBottom: "20px",fontSize: "14px", padding: "15px"};
+const Button = {width: "100%", padding: "10px",backgroundColor: "Green", color: "white", border: "0", cursor: "pointer"};
+const Half = {marginTop: "15px", marginBottom: "15px", display: "flex", alignItems: "Center", justifyContent: "Center"};
+const Horizontal = {width: "100%"};
+const Opacity = {opacity: "0.6"};
+const LogoArrangement = {display: "flex", alignItems: "Center", justifyContent: "space-evenly"};
+const Logo = {width: "100px", height: "100px", cursor: "pointer"};
+const Footer = {textAlign: "Center", margin: "45px", opacity: "0.8"};
+
+export const Styles = {
+    Body : Body,
+    Body1 : Body1,
+    Title : Title,
+    Container : Container,
+    LoginTitle : LoginTitle,
+    BodyContainer : BodyContainer,
+    Input : Input,
+    Button : Button,
+    Half : Half,
+    Horizontal : Horizontal,
+    Opacity : Opacity,
+    LogoArrangement : LogoArrangement,
+    Logo : Logo,
+    Footer : Footer
+};
+
